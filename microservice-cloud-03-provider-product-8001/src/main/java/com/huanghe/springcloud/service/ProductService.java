ca92adfc77489cb6ca8bd172bd3d406c3aa8b0c7
huanghe <735597346@qq.com>
2021-10-29 09:12:09 +0800

    Initial commit

---
+++ microservice-cloud-03-provider-product-8001/src/main/java/com/huanghe/springcloud/service/ProductService.java
@@ -0,0 +1,19 @@
+package com.huanghe.springcloud.service;
+
+
+import com.huanghe.springcloud.entities.Product;
+
+import java.util.List;
+
+/**
+ * @Auther: 梦学谷
+ */
+public interface ProductService {
+
+    boolean add(Product product);
+
+    Product get(Long id);
+
+    List<Product> list();
+
+}
---
+++ microservice-cloud-04-consumer-product-80/src/main/java/com/huanghe/springcloud/ProductConsumer_80.java
@@ -0,0 +1,16 @@
+package com.huanghe.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Auther: 梦学谷
+ */
+@SpringBootApplication
+public class ProductConsumer_80 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductConsumer_80.class, args);
+    }
+
+}
