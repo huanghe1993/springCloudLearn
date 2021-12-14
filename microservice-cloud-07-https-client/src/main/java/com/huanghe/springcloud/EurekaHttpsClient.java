b0a458d4eb7f7bc00f9e912fc14e23eb090a49a9
huanghe <735597346@qq.com>
2021-12-14 08:23:37 +0800

    123

--- microservice-cloud-07-https-client/src/main/java/com/huanghe/springcloud/EurekaHttpsClient.java
+++ microservice-cloud-07-https-client/src/main/java/com/huanghe/springcloud/EurekaHttpsClient.java
@@ -4,6 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+/**
+ * https://blog.csdn.net/qq_32238611/article/details/121568007
+ * https://www.codesheep.cn/2018/12/10/eureka-server-https/
+ * https://juejin.cn/post/6844903734804217864
+ * https://zhuanlan.zhihu.com/p/341857389
+ */
 @EnableEurekaClient //将此服务注册到服务注册中心
 @SpringBootApplication
 public class EurekaHttpsClient {
